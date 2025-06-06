cf069091a4fcfabb4620f643572f82cefdb15fb5
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-06-05 18:16:23 -0700

    Tailwind fix

--- kaleyportfolio2025/src/components/Intro.tsx
+++ kaleyportfolio2025/src/components/Intro.tsx
@@ -1,18 +1,15 @@
 import meinChair from '../assets/IMG_0902.jpeg';
 function Intro() {
     return  (
-        // <div> 
-        //     {/* Right */}
-        //     <div> 
-        //         <p> Im <span className='underline'> Kaley Vuong </span></p>
-        //         <p> This is my website </p>
-        //     </div>
-        //     {/* Left */}
-        //     <img src={meinChair} className='w-50 h-20'/>
-        // </div>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <div> 
+            {/* Right */}
+            <div> 
+                <p> Im <span className='underline'> Kaley Vuong </span></p>
+                <p> This is my website </p>
+            </div>
+            {/* Left */}
+            <img src={meinChair} className='max-w-10'/>
+        </div>
 
     )
 }
